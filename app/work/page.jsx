e84ce673cea46faf1696181ca44e2510c283346d
Renaldi Fauzi Adnan <renaldikun299@gmail.com>
2025-02-11 04:05:48 +0700

    11/2/24

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,10 +1,123 @@
-import React from "react";
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "1",
+    category: "frontend",
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    stack: [{ name: "HTML 5" }, { name: "CSS 3" }, { name: "Javascript" }],
+    image: "/assets/work/image_project1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "2",
+    category: "frontend",
+    title: "Project 2",
+    description: "This is a description of project 1.",
+    stack: [{ name: "React" }, { name: "Next.js" }, { name: "Tailwind.css" }],
+    image: "/assets/work/image_project1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "3",
+    category: "frontend",
+    title: "Project 3",
+    description: "This is a description of project 1.",
+    stack: [{ name: "HTML 5" }, { name: "CSS 3" }, { name: "Javascript" }],
+    image: "/assets/work/image_project1.png",
+    live: "",
+    github: "",
+  },
+];
 
 const Work = () => {
+  const [project, setProject] = useState(projects[0]);
   return (
-    <section className="h-full">
-      <div className="container mx-auto">work</div>
-    </section>
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[40px] h-[50%]">
+              {/* Garis Num */}
+              <div className="text-8xl loading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2 className="text-[42px] font-bold loading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category}
+              </h2>
+              <p className="text-white/60 ">{project.description}</p>
+              {/* project item stack */}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              {/* border */}
+              <div className="border border-white/20"></div>
+              {/* button */}
+              <div className="flex items-start gap-4">
+                {/* Live Project */}
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/* Live Project */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github Repo</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full">slider</div>
+        </div>
+      </div>
+    </motion.section>
   );
 };
 
